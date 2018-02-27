8667cfa160cb16d4a33362753d184446e0897598
Asrofi Ridho <Asrofi Ridho>
2018-02-28 00:45:05 +0700

    change mysql host

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -39,7 +39,7 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:mysql://127.0.0.1:3306/picsell"
+            url = "jdbc:mysql://localhost:3306/picsell"
             username = "root"
             password = ""
 
